5bbd83278b733cbaa7b408e69fca5fae2fc39465
Juan Valenzuela <jvalenzuela1994@gmail.com>
2018-11-07 18:00:41 -0600

    adding file what failed to commit
    Done on behalf of Devan

---
+++ Implementation/AwesomePokerGameSln/Code/PGNetwork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace AwesomePokerGameSln.Code
+{
+    public class PGNetwork : Socket
+    {
+        //Constructor
+        public  PGNetwork() : base(SocketType.Stream, ProtocolType.Tcp)
+        {
+            Socket netsock = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public static void Connect(PGNetwork netsock)
+        {
+            try
+            {
+                //Tries to connect. Currently using google as a placeholder
+                netsock.Connect("Google.com", 8888);
+            }
+            catch(SocketException)
+            {
+                //If Connect fails it throws an exception
+                Console.WriteLine("Error: Connection Failed.");
+                return;
+            }
+            return;
+        }
+
+        public static void Disconnect(PGNetwork netsock)
+        {
+            //Make sure there is a connection before we try to close a
+            //non existant connection.
+            if (netsock.Connected == true)
+            {
+                netsock.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
